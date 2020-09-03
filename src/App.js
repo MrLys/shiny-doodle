e78013db4688cd34c4aa4d657440df441a231a86
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2020-09-03 21:52:47 +0200

    Add header with icon

--- src/App.js
+++ src/App.js
@@ -3,38 +3,12 @@ import './App.css';
 import HomePage from "./page/homepage/homepage.component";
 import { Switch, Route, Link, withRouter } from "react-router-dom";
 import ShopPage from "./page/shop-page/shop.component";
+import Header from "./components/header/header.component";
 
-const HatsPage = (props) => (
-	<div>
-		<h1>Hats</h1>
-	</div>
-);
-
-const JacketsPage= () => (
-	<div>
-		<h1>Hats</h1>
-	</div>
-);
-
-const topicsList = (props) => (
-        <div>
-            <h1>Topics list</h1>
-            <Link to={`${props.match.url}/1`}>Topic 1</Link>
-            <Link to={`${props.match.url}/2`}>Topic 2</Link>
-        </div>
-);
-
-
-const topicDetail = (props) => {
-	return(
-		<div>
-			<h1>{`Topic ${props.match.params.topicId}`}</h1>
-		</div>
-	)
-};
 function App() {
   return (
       <div>
+          <Header />
           <Switch>
               <Route exact path='/' component={HomePage} />
               <Route exact path='/shop' component={ShopPage} />
