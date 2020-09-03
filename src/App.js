27efe0f06d28bf1761323695ceac27c76f8d1037
Thomas Lyseggen <thomas.lyseggen@tripletex.no>
2020-09-03 13:35:20 +0200

    Add routing progress

--- src/App.js
+++ src/App.js
@@ -1,11 +1,28 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./page/homepage/homepage.component";
+import { Switch, Route } from "react-router-dom";
+
+const HatsPage = (props) => (
+	<div>
+		<h1>Hats</h1>
+	</div>
+);
+
+const JacketsPage= () => (
+	<div>
+		<h1>Hats</h1>
+	</div>
+);
 
 function App() {
   return (
   	<div>
-  		<HomePage />
+		<Switch>
+			<Route exact path='/' component={HomePage} />
+			<Route path='/hats' component={HatsPage} />
+			<Route path='/jackets' component={JacketsPage} />
+		</Switch>
 	</div>
   );
 }
