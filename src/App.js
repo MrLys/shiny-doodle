5dba70ff7633154d81fcb3c9ca433b13b9634e8d
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2020-09-03 21:04:37 +0200

    Add routing and onClick navigation for menu-items

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import HomePage from "./page/homepage/homepage.component";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, withRouter } from "react-router-dom";
 
 const HatsPage = (props) => (
 	<div>
@@ -15,15 +15,33 @@ const JacketsPage= () => (
 	</div>
 );
 
+const topicsList = (props) => (
+        <div>
+            <h1>Topics list</h1>
+            <Link to={`${props.match.url}/1`}>Topic 1</Link>
+            <Link to={`${props.match.url}/2`}>Topic 2</Link>
+        </div>
+);
+
+
+const topicDetail = (props) => {
+	return(
+		<div>
+			<h1>{`Topic ${props.match.params.topicId}`}</h1>
+		</div>
+	)
+};
 function App() {
   return (
-  	<div>
-		<Switch>
-			<Route exact path='/' component={HomePage} />
-			<Route path='/hats' component={HatsPage} />
-			<Route path='/jackets' component={JacketsPage} />
-		</Switch>
-	</div>
+      <div>
+          <Switch>
+              <Route exact path='/' component={HomePage} />
+              <Route path='/hats' component={HatsPage} />
+              <Route path='/jackets' component={JacketsPage} />
+              <Route exact path='/topic' component={topicsList} />
+              <Route path='/topic/:topicId' component={topicDetail} />
+          </Switch>
+      </div>
   );
 }
 
