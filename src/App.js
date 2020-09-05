73c37148e6bc0b9d6a905537a28f05419aa53040
Thomas Fossøy Lyseggen <thomas@lyseggen.xyz>
2020-09-05 16:25:17 +0200

    Add firebase and google signing

--- src/App.js
+++ src/App.js
@@ -5,18 +5,40 @@ import { Switch, Route } from "react-router-dom";
 import ShopPage from "./page/shop-page/shop.component";
 import Header from "./components/header/header.component";
 import SignInAndUpPage from "./page/sign-in-and-up/sign-in-and-up.component";
+import { auth } from "./firebase/firebase.util";
 
-function App() {
-  return (
-      <div>
-          <Header />
-          <Switch>
-              <Route exact path='/' component={HomePage} />
-              <Route exact path='/shop' component={ShopPage} />
-              <Route exact path='/signin' component={SignInAndUpPage} />
-          </Switch>
-      </div>
-  );
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            currentUser: null
+        }
+    }
+
+    componentDidMount() {
+       auth.onAuthStateChanged(user => {
+           this.setState({currentUser: user});
+       });
+    }
+
+    unsubscribeFromAuth = null;
+
+    componentWillUnmount() {
+        this.unsubscribeFromAuth();
+    }
+
+    render() {
+        return (
+            <div>
+                <Header signedIn={this.state.currentUser}/>
+                <Switch>
+                    <Route exact path='/' component={HomePage}/>
+                    <Route exact path='/shop' component={ShopPage}/>
+                    <Route exact path='/signin' component={SignInAndUpPage}/>
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -2,7 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './header.styles.scss'
 import { ReactComponent as Logo } from '../../assets/crown.svg';
-const Header = () => {
+
+import { auth } from '../../firebase/firebase.util';
+
+const Header = ({signedIn}) => {
     return (
         <div className='header'>
             <Link to="/" >
@@ -11,7 +14,13 @@ const Header = () => {
             <div className='options'>
                 <Link className='option' to="/shop">Shop</Link>
                 <Link className='option' to="/contact">Contact</Link>
-                <Link className='option' to="/signin">Sign In</Link>
+                {
+                    signedIn ?
+                        (<div className='option' onClick={() => auth.signOut()}>
+                            SIGN OUT
+                        </div>) :
+                        (<Link className='option' to="/signin">SIGN IN</Link>)
+                }
             </div>
         </div>
     )
